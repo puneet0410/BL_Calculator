6d31c893c122b4e0ea5b48fb20858304b72c9729
puneet0410 <puneetp2200@gmail.com>
2025-01-17 12:22:09 +0530

    division f

---
+++ division.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class division {
+    public static void main(String[] args) {
+       
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+
+        
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+
+        double sum = num1 / num2;
+
+      
+        System.out.println("The subtraction of " + num1 + " and " + num2 + " is: " + sum);
+
+        // Close the scanner
+        scanner.close();
+    }
+}
